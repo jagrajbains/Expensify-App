6795ef0ed4323d5938af15bdbbfb3f70eee1042a
Jagraj Bains <jagrajbains.94@gmail.com>
2019-06-17 12:22:10 +0530

    Updating the code

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,21 +1,38 @@
 import uuid from "uuid";
+import database from "../firebase/firebase";
 
 //     1. ADD_EXPENSE
-export const addExpense = ({
-  description = "",
-  note = "",
-  amount = 0,
-  createdAt = 0
-} = {}) => ({
+export const addExpense = expense => ({
   type: "ADD_EXPENSE",
-  expense: {
-    id: uuid(),
-    description,
-    note,
-    amount,
-    createdAt
-  }
+  expense
 });
+/**
+ * By default redux doesn't allow us to export a function
+ * Since we have integrated redux-thunk and we are using the thunkMiddleware
+ * we will be able to dispatch a callback function
+ */
+export const startAddExpense = (expenseData = {}) => {
+  return dispatch => {
+    const {
+      description = "",
+      note = "",
+      amount = 0,
+      createdAt = 0
+    } = expenseData;
+    const expense = { description, note, amount, createdAt };
+    return database
+      .ref("Expenses")
+      .push(expense)
+      .then(ref => {
+        dispatch(
+          addExpense({
+            id: ref.key,
+            ...expense
+          })
+        );
+      });
+  };
+};
 //     2. REMOVE_EXPENSE
 export const removeExpense = ({ id = 0 } = {}) => ({
   type: "REMOVE_EXPENSE",
